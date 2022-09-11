646cddc3129e071109f6fdbef27476ad8a24ebfb
iambhagath <edarend99@gmail.com>
2022-09-11 10:27:11 +0530

    .

---
+++ react_apps/index.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>hello from js</h1>, document.getElementById("root"))
+ReactDOM.render(
+<ul>
+<h1>List 1</h1>
+    <li>als</li>
+    <li>alds</li>
+
+</ul>
+, document.getElementById("root"))
+
